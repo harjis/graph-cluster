0215b612e70895d6f11b4c2562f539e7a65a0596
Joonas Harjukallio <harjukallio@gmail.com>
2020-01-30 14:56:54 +0200

    Add routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :api do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'graphs/:id/reset', to: 'graphs#reset'
+    get 'graphs/:id/undo', to: 'graphs#undo'
+    get 'graphs/:id/calculate_node_count', to: 'graphs#calculate_node_count'
+    get 'graphs/get_node_count', to: 'graphs#get_node_count'
+    resources :graphs do
+      resources :nodes
+      resources :edges
+    end
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
 end
