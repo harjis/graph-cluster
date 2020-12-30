df4a4674bc7c7262e97ac878aca29a6ebafaa807
Joonas Harjukallio <harjukallio@gmail.com>
2020-12-30 11:44:06 +0200

    can deleteEdge

--- frontend/src/features/DataGraph/hooks/useEdgeState.ts
+++ frontend/src/features/DataGraph/hooks/useEdgeState.ts
@@ -1,11 +1,14 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
+import { currentGraphIdQuery } from '../atoms/graph';
+import { destroyEdge } from '../../../api/edges';
 import { edgeIdsState, edgeState } from '../atoms/edges';
-import { nodeState } from '../atoms/nodes';
 import {
   getNodeBottomMiddlePosition,
   getNodeTopMiddlePosition,
 } from '../utils/nodeUtils';
+import { nodeState } from '../atoms/nodes';
+import { useAsyncEffect } from '../../../hooks/useAsyncEffect';
 
 type Coordinates = { x: number; y: number };
 type Props = {
@@ -17,16 +20,20 @@ type Return = {
   toCoordinates: Coordinates;
 };
 export const useEdgeState = (props: Props): Return => {
+  const currentGraphId = useRecoilValue(currentGraphIdQuery);
   const setEdgeIdsState = useSetRecoilState(edgeIdsState);
   const edge = useRecoilValue(edgeState(props.edgeId));
   const toNode = useRecoilValue(nodeState(edge.to_node_id));
   const fromNode = useRecoilValue(nodeState(edge.from_node_id));
 
-  const deleteEdge = () => {
-    setEdgeIdsState((edgeIds) =>
-      edgeIds.filter((edgeId) => edgeId !== edge.id)
-    );
-  };
+  const deleteEdge = useAsyncEffect(async (isMounted) => {
+    const wasDeleteSuccess = await destroyEdge(currentGraphId, props.edgeId);
+    if (isMounted() && wasDeleteSuccess) {
+      setEdgeIdsState((edgeIds) =>
+        edgeIds.filter((edgeId) => edgeId !== edge.id)
+      );
+    }
+  });
 
   return {
     deleteEdge,
