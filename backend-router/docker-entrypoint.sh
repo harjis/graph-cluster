dc045f4b8ee55ef6e9049c10e53d898be114008d
Joonas Harjukallio <harjukallio@gmail.com>
2021-01-27 04:38:41 +0200

    Docs

--- backend-router/docker-entrypoint.sh
+++ backend-router/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Set-Builtin
 set -eu
 
 envsubst '${BACKEND_CLUSTER_IP_SERVICE} ${BACKEND_CLUSTER_IP_SERVICE_PORT} ${FOLDER_SERVICE_BACKEND_CLUSTER_IP_SERVICE} ${FOLDER_SERVICE_BACKEND_CLUSTER_IP_SERVICE_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
