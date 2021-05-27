c4362194568c6d94a463b0741b572cb18e8c8db8
Joonas Harjukallio <harjukallio@gmail.com>
2021-05-27 13:55:37 +0300

    Update useWindowEventListener

--- frontend/src/hooks/useWindowEventListener.ts
+++ frontend/src/hooks/useWindowEventListener.ts
@@ -1,27 +1,18 @@
-import { useEffect, useRef } from 'react';
+import {useEffect} from 'react';
 
-export type Handler<E extends keyof WindowEventMap> = (
-  e: WindowEventMap[E]
-) => void;
+export type Handler<E extends keyof WindowEventMap> = (e: WindowEventMap[E]) => void;
 
-// TODO make this work with refs and other elements too
 export const useWindowEventListener = <EventName extends keyof WindowEventMap>(
-  eventName: EventName,
-  handler: Handler<EventName>
-) => {
-  const savedHandler = useRef<Handler<EventName>>();
+    eventName: EventName,
+    handler: Handler<EventName>,
+): void => {
   useEffect(() => {
-    savedHandler.current = handler;
-  }, [handler]);
-
-  useEffect(() => {
-    const eventListener = (event: WindowEventMap[EventName]) =>
-      savedHandler.current && savedHandler.current(event);
+    const eventListener = (event: WindowEventMap[EventName]) => handler(event);
 
     window.addEventListener(eventName, eventListener);
 
     return () => {
       window.removeEventListener(eventName, eventListener);
     };
-  }, [eventName]);
+  }, [eventName, handler]);
 };
